30dc5c571dc7bbfea57c1a9fd608a359468dc0a2
Тревогина София Геннадьевна <s.trevogina@univ.priv>
2026-09-19 09:34:40 +0300

    init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleApp1.Generation;
+using ConsoleApp1.Generation.AbstractFactory;
+using ConsoleApp1.Generation.FactoryMenhod;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Singlton.Run();
+            //Prototype.Run();
+            //FactoryMethod.Run();
+            AbstractFactory.Run();
+        }
+    }
+}
