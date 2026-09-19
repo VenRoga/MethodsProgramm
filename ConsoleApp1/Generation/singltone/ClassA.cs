c9414250a1003b56f03e777e1e5e539a98890f03
m.novokshonov <m.novokshonov@PC213-22>
2026-09-19 11:27:52 +0300

    add ILogger

--- ConsoleApp1/Generation/singltone/ClassA.cs
+++ ConsoleApp1/Generation/singltone/ClassA.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleApp1.Generation.FactoryMenhod;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,14 +7,23 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1.Generation
 {
-    internal class ClassA
+    internal class ClassA : ILogger
     {
+        ILogger ILogger;
+
+
+
         public ClassA()
-        {
+        {    
             Logger log = Logger.GetInstance();
             log.Log("Class A was init");
 
             Console.WriteLine(log.GetHashCode());
+        }
+
+        public void Log(string message)
+        {
+
         }
     }
 }
--- ConsoleApp1/Generation/singltone/ClassB.cs
+++ ConsoleApp1/Generation/singltone/ClassB.cs
@@ -1,19 +1,29 @@
-﻿using System;
+﻿using ConsoleApp1.Generation.FactoryMenhod;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1.Generation
 {
-    internal class ClassB
+    internal class ClassB : ILogger
     {
+        ILogger Ilogger;
+
+
         public ClassB()
         {
             Logger log = Logger.GetInstance();
             log.Log("Class B was init");
 
             Console.WriteLine(log.GetHashCode());
+        }
+
+        public void Log(string message) 
+        {
+
         }
     }
 }
